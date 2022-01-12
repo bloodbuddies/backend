f745435445bb49a013cc5d6fc20950850b9dc77a
Ritik Chourasiya <ritvikchoure65@gmail.com>
2022-01-12 23:07:10 +0530

    First Commit

---
+++ controllers/auth.controller.js
@@ -0,0 +1,199 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config/config');
+const crypto = require('crypto');
+const Donor = require('../models/donor.model');
+const Patient = require('../models/patient.model');
+const LoginActivityModel = require('../models/loginActivity.model');
+const Joi = require('joi');
+const bcrypt = require('bcryptjs');
+const sendMail = require('../utils/sendMail');
+const { generateotp, fast2sms } = require('../utils/sendOTP');
+const schema = require('../utils/validationSchema.js');
+
+module.exports = {
+  emailLogin, generateOtp, verifyOTP, updateMe, logout
+}
+
+async function emailLogin(req) {
+  const { email, password } = req.body;
+  let userDetails = { email: req.body.email, password: req.body.password }
+  userDetails = await Joi.validate(userDetails, schema.userLoginSchema, { abortEarly: true });
+
+  var user = await Donor.findOne({ email }).select('+password');
+  if(!user) return false;
+
+  
+  const isPasswordMatched = await user.comparePassword(password);
+  console.log(isPasswordMatched)
+  if(!isPasswordMatched) return false;
+
+  if(user.donatedAt != null ) await user.checkAvailabilityStatus(user);
+  loginActivityDetails = { user_id: user._id, user_type: "donor", body: req.body, status: 'success', };
+  loginActivityDetails = await loginActivity(loginActivityDetails);
+
+  token = await generateToken(user);
+
+  return { user, token };
+}
+
+async function generateOtp(req) {
+  const validation = await Joi.validate(req.body, schema.loginOtpSchema, { abortEarly: true });
+  const { mobile } = req.body;
+
+  var user = await Donor.findOne({ mobile });
+  if(!user) 
+    var user = await Patient.findOne({ mobile });
+
+  if(!user) return false;
+
+  const otp = generateotp(6); // generating otp
+  user.mobileOtp = otp;
+  user.mobileOtpExpire = Date.now() + 10 * 60 * 1000;
+  console.log(user);
+  await user.save();
+
+  // await fast2sms(
+  //   {
+  //     message: `Your OTP is ${otp}`,
+  //     contactNumber: user.mobile,
+  //   },
+  // );
+
+  return true;
+}
+
+async function verifyOTP(req) {
+  let userDetails = { mobile: req.body.mobile, otp: req.body.otp }
+  userDetails = await Joi.validate(userDetails, schema.verifyOtpSchema, { abortEarly: true });
+
+  var user = await Donor.findOne({ mobile: userDetails.mobile });
+  if(!user) 
+  var user = await Patient.findOne({ mobile: userDetails.mobile }); var type = "patient"
+
+  if(!user) return false;
+
+  if(user.mobileOtpExpire > Date.now() && user.mobileOtp == userDetails.otp){
+    console.log(user)
+    
+    if(user.donatedAt === null || user.donatedAt != null)
+    {
+      if(user.donatedAt != null ) await user.checkAvailabilityStatus(user);
+      loginActivityDetails = { user_id: user._id, user_type: "donor", body: req.body, status: 'success', };
+      loginActivityDetails = await loginActivity(loginActivityDetails);
+    }else{
+      loginActivityDetails = { user_id: user._id, user_type: "patient", body: req.body, status: 'success', };
+      loginActivityDetails = await loginActivity(loginActivityDetails);
+    }
+    
+    user.mobileOtp = "";
+    user.mobileOtpExpire = "";
+    await user.save();
+    token = await generateToken(user);
+    
+    return { user, token };
+  } else
+  {
+    if(user.donatedAt === null || user.donatedAt != null)
+    {
+      if(user.donatedAt != null ) await user.checkAvailabilityStatus(user);
+      loginActivityDetails = { user_id: user._id, user_type: "donor", body: req.body, status: 'failed', };
+      loginActivityDetails = await loginActivity(loginActivityDetails);
+    }else{
+      loginActivityDetails = { user_id: user._id, user_type: "patient", body: req.body, status: 'failed', };
+      loginActivityDetails = await loginActivity(loginActivityDetails);
+    }
+
+    return false;
+  }
+}
+
+function generateToken(user) {
+  return jwt.sign({user}, config.jwtSecret, { expiresIn: "30s"});
+}
+
+async function loginActivity(req) {
+  const { user_id, user_type } = req;
+  const { agent, ip, latitude, longitude } = req.body;
+  const login_status = req.status;
+  const details = { user_id, user_type, agent, ip, latitude, longitude, login_status }
+  const act = await new LoginActivityModel(details).save();
+  console.log(act);
+}
+
+async function updateMe(user) {
+  const validation = await Joi.validate(user.body, schema.updateMeSchema, { abortEarly: true });
+  const updateMe = await User.findByIdAndUpdate(user.id, user.body, {new: true, runValidators: true, useFindAndModify:false});
+  return updateMe;
+}
+
+// Logout API Function
+async function logout(req) {
+  if(req.user)
+  {
+    const query = {user: req.user._id, login_status: "success"};
+
+    let loggedIn = await LoginActivityModel.findOne(query).sort({_id:-1}).limit(1);
+    
+    loggedIn.logout_time = new Date;
+    await loggedIn.save();
+    
+    return true;
+  } else
+    return false;
+}
+
+//-------------------------------------------------------------------------------------------------------------------------
+
+// Generating Password Reset Token -- ritik
+// async function sendResetPasswordToken(user) {
+//   const userEmail = await Joi.validate(user, forgetPasswordSchema, { abortEarly: true });
+//   const getUser = await User.findOne({email: userEmail.email});
+//   if(!getUser) { return false; }
+
+//   //Generating Token
+//   const resetToken = crypto.randomBytes(20).toString("hex");
+
+//   //Hashing & Adding resetPasswordToken to userSchema
+//   getUser.resetPasswordToken = crypto.createHash("sha256").update(resetToken).digest("hex");
+//   getUser.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
+//   await getUser.save({validateBeforeSave:false});
+
+//   const resetPasswordUrl = `http://localhost:3000/api/auth/password/reset/${resetToken}`;
+//   const message = `Your Password Reset Token is :- \n\n ${resetPasswordUrl} \n\n If you have not requested this email then, please ignore it`;
+
+//   // Sending Mail
+//   try
+//   {
+//       await sendMail({
+//           email: getUser.email,
+//           subject: `RabiesLMS Password Reset`,
+//           message,
+//       })
+//       return true;
+//   }
+//   catch(err)
+//   {
+//       getUser.resetPasswordToken = undefined;
+//       getUser.resetPasswordExpire = undefined;
+
+//       await getUser.save({validateBeforeSave:false});
+
+//       return false;
+//   }
+// }
+
+// Reset Password After Getting Forget Password URL -- ritik
+// async function resetPassword(data) {
+//   const resettoken = crypto.createHash("sha256").update(data.token).digest("hex");
+//   const user = await User.findOne({resetPasswordToken: resettoken});
+
+//   if(!user) { return null; }
+//   if(user.resetPasswordExpire < Date.now()) { return false; }
+
+//   user.hashedPassword = await bcrypt.hashSync(data.password, 10);
+//   user.resetPasswordToken = undefined;
+//   user.resetPasswordExpire = undefined;
+
+//   await user.save();
+//   return user;
+// }
---
+++ routes/auth.route.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler')
+const passport = require('passport');
+const authCtrl = require('../controllers/auth.controller');
+const config = require('../config/config');
+const customResponse = require('../middleware/response-handler');
+const router = express.Router();
+const Joi = require('joi');
+const Donor = require('../models/donor.model');
+const {resMsgType, resMsg} = require('../config/constant');
+module.exports = router;
+
+// ------------------ Validation Schemas (Start) ------------------
+const resetPasswordSchema = Joi.object({
+  password: Joi.string().required(),
+  repeat_password: Joi.string().required().valid(Joi.ref('password')),
+});
+
+
+// ------------------ Validation Schemas (End) ------------------
+
+// ------------------ Auth Related Routes (Start) ------------------
+router.post('/login', asyncHandler(emailLogin)); // Done - ( FIX ME Later)
+router.post('/generateotp', asyncHandler(generateOTP)); // Done
+router.post('/verifyotp', asyncHandler(verifyOTP)); // Done
+router.post('/logout', passport.authenticate('jwt', { session: false }), asyncHandler(logout)); 
+
+// router.get('/updateprofile', passport.authenticate('jwt', { session: false }), updateProfile);
+// ------------------ Auth Related Routes (End) ------------------
+
+
+async function emailLogin(req, res, next) {
+  try{
+
+    // Details for LoginActivity
+    var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
+    let agent = req.headers['user-agent'];
+  
+    if(!ip && !agent){ return customResponse(res,400, resMsgType.WARNING,resMsg.NO_DATA_FOUND, null); }
+  
+    req.body.agent = agent;
+    req.body.ip = ip;
+
+    let donorLogin = await authCtrl.emailLogin(req);
+    if(!donorLogin) return customResponse(res,401, resMsgType.WARNING,resMsg.LOGIN_FAILED, null);
+    return customResponse(res,201, resMsgType.SUCCESS,resMsg.LOGIN, donorLogin);
+  }catch(e){
+    console.log(e)
+    return customResponse(res,500, resMsgType.ERROR,resMsg.SWR, e.message);
+  }
+}
+
+// Route for Generate OTP for Login
+async function generateOTP(req, res, next) {
+  try{
+    let generateOtp = await authCtrl.generateOtp(req);
+    if(!generateOtp) return customResponse(res,500, resMsgType.ERROR,resMsg.NO_DATA_FOUND, "Try Again");
+    return customResponse(res,201,resMsgType.SUCCESS,resMsg.OTP, null);
+  }catch(e){
+    return customResponse(res,500, resMsgType.ERROR,resMsg.SWR, e.message);
+  }
+}
+
+
+// Route for verify OTP for Login
+async function verifyOTP(req, res, next) {
+  try{
+
+    // Details for LoginActivity
+    var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
+    let agent = req.headers['user-agent'];
+  
+    if(!ip && !agent){ return customResponse(res,400, resMsgType.WARNING,resMsg.NO_DATA_FOUND, null); }
+  
+    req.body.agent = agent;
+    req.body.ip = ip;
+
+    let isOtpVerified = await authCtrl.verifyOTP(req);
+    if(!isOtpVerified) return customResponse(res,403, resMsgType.SUCCESS,resMsg.LOGIN_FAILED, null);
+    return customResponse(res,201, resMsgType.SUCCESS,resMsg.LOGIN, isOtpVerified);
+  }catch(e){
+    return customResponse(res,500, resMsgType.ERROR,resMsg.SWR, e.message);
+  }
+}
+
+async function logout(req, res, next) {
+  try{
+      // console.log(req.user)
+      let logout = await authCtrl.logout(req);
+      if(!logout) { customResponse(res,400, resMsgType.WARNING,resMsg.BAD_REQUEST, null); }
+      return customResponse(res,200, resMsgType.SUCCESS,resMsg.LOGOUT, null);
+  }catch(e){
+      customResponse(res,500, resMsgType.ERROR,resMsg.SWR, e.message);
+  }
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------------
+// router.post('/forgetpassword', asyncHandler(forgetPassword)); 
+// router.put('/password/reset/:token', asyncHandler(resetPassword), donorLogin); 
+
+// async function resetPassword(req, res, next) {
+//   const validation = await Joi.validate(req.body, resetPasswordSchema, { abortEarly: true });
+//   const data = { token: req.params.token, password: req.body.password };
+//   let user = await authCtrl.resetPassword(data);
+//   if(user === null) { customResponse(res,400, resMsgType.WARNING,resMsg.NO_DATA_FOUND, null);}
+//   if(user === false) { return customResponse(res,500, resMsgType.ERROR,resMsg.TOKEN_EXPIRED, null); }
+
+//   req.user = user;
+//   next()
+// }
